256fdac9d615406e19300ba8b1850b63afd22ae0
Arjun Singh Negi <arjunnegi352006@gmail.com>
2026-02-22 19:14:27 +0530

    login with secure password verification

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request
-from werkzeug.security import generate_password_hash
+from flask import Flask, render_template, request,redirect, url_for, session
+from werkzeug.security import generate_password_hash, check_password_hash
 import pyotp
 from database import get_db_connection
 app=Flask(__name__)
@@ -35,5 +35,26 @@ def register():
         return "Registration successful"
 
     return render_template("register.html")
+
+@app.route("/login",methods=["GET","POST"])
+def login():
+    if request.method=="POST":
+        email=request.form["email"]
+        password=request.form["password"]
+
+        conn=get_db_connection()
+        cursor=conn.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM users WHERE email=%s", (email,))
+        user=cursor.fetchone()
+        conn.close()
+
+        if user and check_password_hash(user["password"],password):
+            session["temp_user_id"]=user["id"]
+            return "password verified. Proceed to OTP."
+        
+        return "Invalid email or password"
+    
+    return render_template("login.html")
+
 if __name__=="__main__":
     app.run(debug=True)
